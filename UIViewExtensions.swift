8477af92154ca81f2aa085de0feede5dd597a195
Eric Theriault <eric.m.theriault@gmail.com>
2019-11-02 14:11:09 -0500

    Add border and rounding extensions

--- UIViewExtensions.swift
+++ UIViewExtensions.swift
@@ -21,4 +21,36 @@ extension UIView {
             ])
     }
 
+    func addGradient(startColor: UIColor, endColor: UIColor, isHorizontal: Bool = false) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
+        if isHorizontal {
+            gradient.startPoint = CGPoint(x: 0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        }
+        layer.insertSublayer(gradient, at: 0)
+    }
+
+    func clipToCircle() {
+        let diameter = min(frame.width, frame.height)
+        roundCorners(withRadius: diameter * 0.5)
+    }
+
+    func roundCorners(withRadius radius: CGFloat) {
+        layer.masksToBounds = true
+        layer.cornerRadius = radius
+    }
+
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let mask = CAShapeLayer()
+        mask.path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+        layer.mask = mask
+    }
+
+    func addBorder(width: CGFloat, color: UIColor) {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+    }
+
 }
